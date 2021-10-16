1678619e76e2ff1bee66a4e35e3f716e7292a66b
ntaheij <noah@taheij.nl>
2021-10-16 23:15:04 +0200

    Made some readability changes

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,11 @@ export default function Home() {
       <div id="hello">
         <h2 className="gradient-text text-md md:text-2xl">Saturday, 16th October 2021, 10:51 PM</h2>
         <h1 className="text-4xl md:text-6xl font-semibold">Good evening, <span className="gradient-text">Noah!</span></h1>
-        <h3 className="text-xl md:text-2xl"><span className="gradient-text">Overcast Clouds</span>, <span className="gradient-text">11</span> degrees, but feels like <span className="gradient-text">9</span> degrees in <span className="gradient-text">Emmeloord</span></h3>
+        <h3 className="mt-1 md:mt-4 text-xl md:text-2xl"><span className="gradient-text">Overcast Clouds</span>, <span className="gradient-text">11</span> degrees, but feels like <span className="gradient-text">9</span> degrees in <span className="gradient-text">Emmeloord</span></h3>
 
         <h4 className="mt-8 text-3xl md:text-4xl font-semibold">Saved <span className="gradient-text">Pages</span></h4>
         
-        <div id="cards" class="md:ml-4 mt-10 mb-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 md:gap-6">
+        <div id="cards" className="md:ml-4 mt-10 mb-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 md:gap-6">
           { 
             items.map((item) => {
               return (
