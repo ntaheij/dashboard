eadaa6a1b73fe7063e147ebad0ea42386fedc74f
ntaheij <noah@taheij.nl>
2021-10-16 21:04:00 +0200

    Initial pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import "tailwindcss/base.css";
+import "tailwindcss/components.css";
+import "tailwindcss/utilities.css";
+import "tailwindcss/screens.css";
+
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        />
+      </Head>
+      <div className="bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-100 min-h-screen" id="App">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 
-export default MyApp
+export default MyApp;
